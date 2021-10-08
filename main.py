3baee664730d6212c312824538c260905f03fb46
slq0 <laquitainesteeve@gmail.com>
2021-10-08 12:57:01 +0000

    Add tf-serving

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
 from src.convnet.pipes import train
 import yaml 
+import sys
+import os 
 
 with open("conf/parameters.yml") as file:
     PARAMS = yaml.safe_load(file)
@@ -8,4 +10,21 @@ with open("conf/parameters.yml") as file:
 
 if __name__ == "__main__":
     
-    train.run(params=PARAMS)
+
+    if sys.argv[1] == "train":
+            
+        # train model
+        train.run(params=PARAMS)
+        
+    elif sys.argv[1] == "serve":
+
+        # serve model
+        os.environ["MODEL_PATH"] = PARAMS["MODEL_PATH"]
+        os.system(f"""nohup tensorflow_model_server \
+            --rest_api_port=8502 \ 
+            --model_name=img_model \
+                --model_base_path={PARAMS['MODEL_PATH']} >server.log 2>&1"""
+                )
+
+    elif sys.argv[1] == "predict":
+        pass
--- src/convnet/pipes/train.py
+++ src/convnet/pipes/train.py
@@ -1,15 +1,20 @@
 
 from src.convnet.nodes.etl import load_dataset
 from src.convnet.pipes import prep
-from src.convnet.nodes.train import build_model
+from src.convnet.nodes.train import build_model, save_model
 from typing import Dict, Any
 import tensorflow as tf
+import os 
 
 def run(params:Dict[str, Any]):
     
     # define set of image classes
     LABELS = params["LABELS"]
     EPOCHS = params["EPOCHS"]
+    MODEL_PATH = params["MODEL_PATH"]
+    MODEL_VERS = params["VERSION"]
+    MODEL_LOSS = params["MODEL_LOSS"]
+    METRICS = params["METRICS"] 
 
     # etl
     (X_train, Y_train), (X_test, Y_test) = load_dataset(name="fashion_mnist")
@@ -19,9 +24,20 @@ def run(params:Dict[str, Any]):
 
     # training
     model = build_model()
-    model.compile(optimizer=tf.compat.v1.train.AdamOptimizer(),
-                loss="sparse_categorical_crossentropy",
-                metrics=["accuracy"])
+    model.compile(
+        optimizer=tf.compat.v1.train.AdamOptimizer(),
+        loss=MODEL_LOSS,
+        metrics=METRICS
+        )
     model.fit(X_train, Y_train, epochs=EPOCHS)
+    
+    # inspect saved model
+    os.system("saved_model_cli show --dir {save_path} --all")
+
+    # evaluate
     test_loss, test_acc = model.evaluate(X_test, Y_test)
     print(f"\nTest accuracy: {test_acc}")
+
+    # save model
+    save_path = os.path.join(MODEL_PATH, str(MODEL_VERS))
+    save_model(model, save_path)
